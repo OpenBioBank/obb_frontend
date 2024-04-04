886afd7e65011b52deab59b3aa7ef6cc126a3844
My-Don <165189533+My-Don@users.noreply.github.com>
2024-04-04 21:21:46 +0800

    Update WalletDemo.vue
    Signed-off-by: My-Don <165189533+My-Don@users.noreply.github.com>

--- src/components/WalletDemo.vue
+++ src/components/WalletDemo.vue
@@ -18,21 +18,36 @@
 
 
 <script setup lang="ts" name="WalletDemo">
-import { Buffer } from "buffer/";
+import { Buffer } from "buffer";
 window.Buffer = Buffer;
+
+import { BN } from 'bn.js';
+import * as borsh from "borsh";
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+//import  NftAbi  from "../utils/NftAbi";
+
+//import  NftAbiV2  from "../utils/NftAbiV2";
+
+// import {
+//   getAssociatedTokenAddressSync,
+//   ASSOCIATED_TOKEN_PROGRAM_ID,
+//   TOKEN_PROGRAM_ID,
+// } from "@solana/spl-token";
 
 import {
   Connection,
   SystemProgram,
+  SYSVAR_RENT_PUBKEY,
   Transaction,
   TransactionInstruction,
   PublicKey,
 } from "@solana/web3.js";
 // solana mainnet rpc
-const rpc =
-  "https://solana-mainnet.g.alchemy.com/v2/vzFXAOxlxQoAAKZPr9qrVc29rB4HGOJp";
+// const rpc =
+//   "https://solana-mainnet.g.alchemy.com/v2/vzFXAOxlxQoAAKZPr9qrVc29rB4HGOJp";
 // solana devnet rpc
-//const rpc ="https://solana-devnet.g.alchemy.com/v2/OLI1XL9pAdLjtvpt06bqBfdbsdODRn_Y";
+const rpc ="https://solana-devnet.g.alchemy.com/v2/OLI1XL9pAdLjtvpt06bqBfdbsdODRn_Y";
 const connection = new Connection(rpc, "confirmed");
 
 // solana Browser plugin wallet
@@ -64,8 +79,8 @@ const connection = new Connection(rpc, "confirmed");
 
 
 // Deploy solana devnet contract address and abi
-const contractAddress = "";
-const contractAbi = "";
+//const nftAddress = "8UUKMQeG6PfN8wdbU1uViZN4XjZJVfuFFzvpU72oQz7e";
+const nftAddressV2 = "CyGeFwXuqGHVV5HuzJ1iFMRqeL2YxU5CYTXyy5Cpbkg1";
 
 let msg1 = "连接钱包";
 let tr = "交易";
@@ -87,7 +102,18 @@ const getProvider = () => {
 };
 
 const phantom = getProvider();
-console.log("phantom", phantom);
+//console.log("phantom", phantom);
+
+// 合约地址
+//const programId = new PublicKey(nftAddress);
+const programIdV2 = new PublicKey(nftAddressV2);
+console.log("programIdV2: ",programIdV2)
+// 创建合约对象
+// const program = new Program(NftAbi, programId, phantom);
+// console.log("program:  ", program);
+
+// const programV2 = new Program(NftAbiV2, programIdV2, phantom);
+// console.log("programV2:  ", programV2);
 
 // 钱包相关操作
 function walletOperate() {
@@ -183,25 +209,150 @@ async function sign() {
 async function callContract() {
   try {
     // 构建交易
-    const instruction = new TransactionInstruction({
-      keys: [{ pubkey: phantom.publicKey, isSigner: true, isWritable: true }],
-      programId: new PublicKey(contractAddress), // 替换为你的程序ID
-      data: Buffer.from([]), // 替换为你的合约调用数据
-    });
+    //const instruction = new TransactionInstruction({
+    //   keys: [{ pubkey: phantom.publicKey, isSigner: true, isWritable: true }],
+    //   programId: new PublicKey(contractAddress), // 替换为你的程序ID
+    //   data: Buffer.from([]), // 替换为你的合约调用数据
+    // });
+
+    // const transaction = new Transaction().add(instruction);
+    // transaction.feePayer = phantom.publicKey;
+    // // 签名并广播交易
+    // await phantom.signAndSendTransaction(transaction);
+
+    // let ix = new TransactionInstruction({
+    //       programId: program.publicKey,
+    //       keys: [
+    //             { pubkey: mintKeypair.publicKey, isSigner: true, isWritable: true },            // initializer
+    //             { pubkey: mintKeypair.publicKey, isSigner: true, isWritable: true },            // token_mint
+    //             { pubkey: payer.publicKey, isSigner: false, isWritable: true },                 // Mint authority account
+    //             { pubkey: TOKEN_METADATA_PROGRAM_ID, isSigner: false, isWritable: false },      // Token metadata program
+    //             { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },        // System program
+    //             { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },               // Token program
+    //             { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },             // Rent account            
+    //         ],
+          
+    //       data: instructionData.toBuffer()
+    //     });
+
+    // 以下方法会Error: This function requires 'Provider.sendAndConfirm' to be implemented.
+    // const instruction = await program.methods
+    //   .mintTo()
+    //   .accounts({
+    //     payer: phantom.publicKey,
+    //     tokenAccount:  new PublicKey("2Uar6SX4PnvzwwANVnP6vSGHTmZZjxLa25sXbmvKPBg8"),
+    //     owner: phantom.publicKey,
+    //     mint: phantom.publicKey,
+    //     associatedTokenProgram: new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL'),
+    //     systemProgram: SystemProgram.programId,
+    //     tokenProgram: new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
+    //   })
+    //   .instruction();
+
+
+    const instructionData = new InitializeTokenMintArgs({
+            instruction: 0,
+            id: new BN(1),
+            description: "this is a nft",
+            owner: phantom.publicKey,
+            creator: phantom.publicKey,
+            authorize: false,
+            url: "https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/nft.json",
+            cid: new BN(1).toString(),
+            is_mutable: false
+        });
+
+
+    let PDA;
+    PDA = await getPDA();
+    console.log("PDA: ",PDA)
+
+    let instruction = new TransactionInstruction({
+            programId: new PublicKey(nftAddressV2),
+            keys: [
+                { pubkey: phantom.publicKey, isSigner: true, isWritable: true },        // Initializer     
+                { pubkey: phantom.publicKey, isSigner: true, isWritable: true },         // token_mint      
+                { pubkey: phantom.publicKey, isSigner: true, isWritable: false },             // mint_auth             
+                { pubkey: new PublicKey(PDA), isSigner: false, isWritable: true },     // token_metadata     
+                { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },        // System program  
+                { pubkey: new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'), isSigner: false, isWritable: false },       // token_program      
+                { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },      // sysvar_rent
+            ],    
+            data: Buffer.from([instructionData])
+        });
+
+       
 
     const transaction = new Transaction().add(instruction);
+
+    const anyTransaction: any = transaction;
+    // 获取最近的区块交易哈希
+    anyTransaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+
     transaction.feePayer = phantom.publicKey;
+
     // 签名并广播交易
-    await phantom.signAndSendTransaction(transaction);
-   
+    const { signature } = await phantom.signAndSendTransaction(transaction);
+
+    // 获取交易签名
+    await connection.getSignatureStatus(signature);
+    
   } catch (error) {
-    console.warn(error);
+    console.log(error);
     throw new Error(error.message);
   }
 }
+  
+class Assignable {
+    constructor(properties) {
+        Object.keys(properties).map((key) => {
+            return (this[key] = properties[key]);
+        });
+    };
+};
 
-// 查询合约
-function selectContract() {}
+
+class InitializeTokenMintArgs extends Assignable {
+    toBuffer() {
+        return Buffer.from(borsh.serialize(InitializeTokenMintArgsSchema, this));
+    }
+};
+const InitializeTokenMintArgsSchema = new Map([
+    [
+    InitializeTokenMintArgs, {
+            kind: 'struct',
+            fields: [
+                ['instruction', 'u8'],
+                ['id', { kind: 'option', type: 'u64' }],
+                ['description', 'string'],
+                ['owner', 'string'],
+                ['creator', 'string'],
+                ['authorize', 'bool'],
+                ['url', 'string'],
+                ['cid', 'string'],
+                ['is_mutable', 'bool']
+            ]
+        }
+    ]
+]);
+
+
+const programId = new PublicKey(
+    "G1DCNUQTSGHehwdLCAmRyAG8hf51eCHrLNUqkgGKYASj"
+  );
+
+  async function getPDA(){
+    let [pda, bump] = await PublicKey.findProgramAddress(
+    [Buffer.from("metadata")],
+    programId
+  );
+
+    console.log(`bump: ${bump}, pubkey: ${pda.toBase58()}`);
+    return pda.toBase58();
+  }
+  
 </script>
 
 
