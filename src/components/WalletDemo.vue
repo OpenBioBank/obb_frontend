40a4519bd9f53733c849df09720ed1416bb05848
My-Don <165189533+My-Don@users.noreply.github.com>
2024-04-06 10:29:38 +0800

    增加链上操作
    Signed-off-by: My-Don <165189533+My-Don@users.noreply.github.com>

--- src/components/WalletDemo.vue
+++ src/components/WalletDemo.vue
@@ -18,22 +18,15 @@
 
 
 <script setup lang="ts" name="WalletDemo">
-import { Buffer } from "buffer";
-window.Buffer = Buffer;
 
-import { BN } from 'bn.js';
+//import { BN } from 'bn.js';
 import * as borsh from "borsh";
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-//import  NftAbi  from "../utils/NftAbi";
 
-//import  NftAbiV2  from "../utils/NftAbiV2";
-
-// import {
-//   getAssociatedTokenAddressSync,
-//   ASSOCIATED_TOKEN_PROGRAM_ID,
-//   TOKEN_PROGRAM_ID,
-// } from "@solana/spl-token";
+import {
+    TOKEN_PROGRAM_ID,
+    getAssociatedTokenAddress,
+    createAssociatedTokenAccountInstruction,
+  } from "@solana/spl-token";
 
 import {
   Connection,
@@ -43,43 +36,11 @@ import {
   TransactionInstruction,
   PublicKey,
 } from "@solana/web3.js";
-// solana mainnet rpc
-// const rpc =
-//   "https://solana-mainnet.g.alchemy.com/v2/vzFXAOxlxQoAAKZPr9qrVc29rB4HGOJp";
+
 // solana devnet rpc
-const rpc = "https://solana-devnet.g.alchemy.com/v2/OLI1XL9pAdLjtvpt06bqBfdbsdODRn_Y";
+const rpc ="https://solana-devnet.g.alchemy.com/v2/OLI1XL9pAdLjtvpt06bqBfdbsdODRn_Y";
 const connection = new Connection(rpc, "confirmed");
 
-// solana Browser plugin wallet
-// https://phantom.app/
-
-// solana rpc node provider
-// https://www.alchemy.com/
-
-// solana faucet
-// https://faucet.solana.com/
-
-// solana devnet explorer
-// https://solscan.io/?cluster=devnet
-
-// solana mainnet explorer
-// https://solscan.io/
-
-// solana playground
-// https://solana.com/zh/developers/guides/getstarted/hello-world-in-your-browser#deploy-your-program
-
-// solana demo
-// https://github.com/phantom/sandbox/tree/b57fdd0e65ce4f01290141a01e33d17fd2f539b9
-
-// sign In With Solana
-// https://github.com/phantom/sign-in-with-solana
-
-// solana cli deployment contract
-// https://solana.com/zh/developers/guides/getstarted/setup-local-development#3-install-the-solana-cli
-
-
-// Deploy solana devnet contract address and abi
-//const nftAddress = "8UUKMQeG6PfN8wdbU1uViZN4XjZJVfuFFzvpU72oQz7e";
 const nftAddressV2 = "CyGeFwXuqGHVV5HuzJ1iFMRqeL2YxU5CYTXyy5Cpbkg1";
 
 let msg1 = "连接钱包";
@@ -102,18 +63,7 @@ const getProvider = () => {
 };
 
 const phantom = getProvider();
-//console.log("phantom", phantom);
 
-// 合约地址
-//const programId = new PublicKey(nftAddress);
-const programIdV2 = new PublicKey(nftAddressV2);
-console.log("programIdV2: ", programIdV2)
-// 创建合约对象
-// const program = new Program(NftAbi, programId, phantom);
-// console.log("program:  ", program);
-
-// const programV2 = new Program(NftAbiV2, programIdV2, phantom);
-// console.log("programV2:  ", programV2);
 
 // 钱包相关操作
 function walletOperate() {
@@ -125,7 +75,7 @@ function walletOperate() {
     var connect_wallet = phantom.connect();
 
     // 连接后
-    phantom.on("connect", async (publicKey: any) => {
+    phantom.on("connect", async (publicKey) => {
       // 检查连接
       console.log("Phantom已连接: " + phantom.isConnected);
 
@@ -143,11 +93,11 @@ function walletOperate() {
     });
 
     // 切换钱包
-    phantom.on("accountChanged", (publicKey: { toBase58: () => any; }) => {
+    phantom.on("accountChanged", (publicKey) => {
       if (publicKey) {
         console.log(`Switched to account ${publicKey.toBase58()}`);
       } else {
-        phantom.connect().catch((error: any) => {
+        phantom.connect().catch((error) => {
           console.log(error);
         });
       }
@@ -206,119 +156,156 @@ async function sign() {
 }
 
 // 调用合约
-// async function callContract() {
-//   try {
-//     // 构建交易
-//     //const instruction = new TransactionInstruction({
-//     //   keys: [{ pubkey: phantom.publicKey, isSigner: true, isWritable: true }],
-//     //   programId: new PublicKey(contractAddress), // 替换为你的程序ID
-//     //   data: Buffer.from([]), // 替换为你的合约调用数据
-//     // });
+async function callContract() {
+  try {
+   
+    // 步骤一 初始化账户
 
-//     // const transaction = new Transaction().add(instruction);
-//     // transaction.feePayer = phantom.publicKey;
-//     // // 签名并广播交易
-//     // await phantom.signAndSendTransaction(transaction);
+    //cid即上传的json哈希
+    let cid = "QmRQnHPzG7oGy5SNXcMzqPXS2rcveMXQRu42nAFrjFEgzC";
+    cid = cid.slice(0, 32)
 
-//     // let ix = new TransactionInstruction({
-//     //       programId: program.publicKey,
-//     //       keys: [
-//     //             { pubkey: mintKeypair.publicKey, isSigner: true, isWritable: true },            // initializer
-//     //             { pubkey: mintKeypair.publicKey, isSigner: true, isWritable: true },            // token_mint
-//     //             { pubkey: payer.publicKey, isSigner: false, isWritable: true },                 // Mint authority account
-//     //             { pubkey: TOKEN_METADATA_PROGRAM_ID, isSigner: false, isWritable: false },      // Token metadata program
-//     //             { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },        // System program
-//     //             { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },               // Token program
-//     //             { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },             // Rent account            
-//     //         ],
+    const instructionData = new InitializeTokenMintArgs({
+            methods_id: 0,
+            id: 81,
+            description: "this is a nft this is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nft",
+            owner: phantom.publicKey.toBase58(),
+            creator: phantom.publicKey.toBase58(),
+            authorize: true,
+            url: "https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/nft.json",
+            cid,
+            is_mutable: false
+        });
 
-//     //       data: instructionData.toBuffer()
-//     //     });
-
-//     // 以下方法会Error: This function requires 'Provider.sendAndConfirm' to be implemented.
-//     // const instruction = await program.methods
-//     //   .mintTo()
-//     //   .accounts({
-//     //     payer: phantom.publicKey,
-//     //     tokenAccount:  new PublicKey("2Uar6SX4PnvzwwANVnP6vSGHTmZZjxLa25sXbmvKPBg8"),
-//     //     owner: phantom.publicKey,
-//     //     mint: phantom.publicKey,
-//     //     associatedTokenProgram: new PublicKey('ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL'),
-//     //     systemProgram: SystemProgram.programId,
-//     //     tokenProgram: new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
-//     //   })
-//     //   .instruction();
+      const [tokenMint] = PublicKey.findProgramAddressSync(
+      [phantom.publicKey.toBuffer(), Buffer.from(cid)],
+      new PublicKey(nftAddressV2)
+      );
+  
+      const [mintAuth] = PublicKey.findProgramAddressSync(
+        [tokenMint.toBuffer()],
+        new PublicKey(nftAddressV2)
+      );
+      
+      const [metadata] = PublicKey.findProgramAddressSync(
+        [tokenMint.toBuffer(), Buffer.from(cid)],
+        new PublicKey(nftAddressV2)
+      );
 
 
-//     const instructionData = new InitializeTokenMintArgs({
-//             methods: 0,
-//             id: new BN(1),
-//             description: "this is a nft",
-//             owner: phantom.publicKey,
-//             creator: phantom.publicKey,
-//             authorize: false,
-//             url: "https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/nft.json",
-//             cid: "https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/nft.json",
-//             is_mutable: false
-//         });
+    let instruction = new TransactionInstruction({
+            keys: [
+                { pubkey: phantom.publicKey, isSigner: true, isWritable: false },        // Initializer     
+                { pubkey: tokenMint, isSigner: false, isWritable: true },         // token_mint      
+                { pubkey: mintAuth, isSigner: false, isWritable: false },             // mint_auth             
+                { pubkey: metadata, isSigner: false, isWritable: true },     // token_metadata     
+                { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },        // System program  
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },       // token_program      
+                { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },      // sysvar_rent
+            ],    
+            programId: new PublicKey(nftAddressV2),
+            data: instructionData.toBuffer()
+        });
 
+    const transaction = new Transaction().add(instruction);
 
-//     let PDA;
-//     PDA = await getPDA();
-//     console.log("PDA: ",PDA)
+    const anyTransaction: any = transaction;
+    //获取最近的区块交易哈希
+    anyTransaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
 
-//     let instruction = new TransactionInstruction({
-//             programId: new PublicKey(nftAddressV2),
-//             keys: [
-//                 { pubkey: phantom.publicKey, isSigner: true, isWritable: true },        // Initializer     
-//                 { pubkey: phantom.publicKey, isSigner: true, isWritable: true },         // token_mint      
-//                 { pubkey: phantom.publicKey, isSigner: true, isWritable: false },             // mint_auth             
-//                 { pubkey: new PublicKey(PDA), isSigner: false, isWritable: true },     // token_metadata     
-//                 { pubkey: SystemProgram.programId, isSigner: false, isWritable: false },        // System program  
-//                 { pubkey: new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'), isSigner: false, isWritable: false },       // token_program      
-//                 { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },      // sysvar_rent
-//             ],    
-//             data: Buffer.from([instructionData])
-//         });
+   transaction.feePayer = phantom.publicKey;
 
+    // 签名并广播交易
+   const { signature } = await phantom.signAndSendTransaction(transaction);
+    
+   // signature即交易哈希
+   console.log("初始化签名: ", signature)
 
+    // 步骤二 创建NFT
+    const userAta = await getAssociatedTokenAddress(tokenMint, phantom.publicKey);
+    console.log("userAta:  ",userAta)
+    const ataAccount = await connection.getAccountInfo(userAta);
+    console.log("ataAccount: ", ataAccount);
 
-//     const transaction = new Transaction().add(instruction);
+    const blockhashInfo = await connection.getLatestBlockhash();
+  
+  // Create transaction
+  const tx = new Transaction({
+    ...blockhashInfo,
+  });
 
+    if (!ataAccount) {
+    let ataInstruction = createAssociatedTokenAccountInstruction(
+      phantom.publicKey,
+      userAta,
+      phantom.publicKey,
+      tokenMint
+    );
+    tx.add(ataInstruction);
+    console.log("ataInstruction: ",ataInstruction)
+  }
 
+  const instructionData2 = new InitializeTokenMintArgs({
+            methods_id: 1,
+            id: 81,
+            description: "this is a nft this is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nftthis is a nft",
+            owner: phantom.publicKey.toBase58(),
+            creator: phantom.publicKey.toBase58(),
+            authorize: true,
+            url: "https://raw.githubusercontent.com/solana-developers/program-examples/new-examples/tokens/tokens/.assets/nft.json",
+            cid,
+            is_mutable: false
+        });
+  
+  let createInstruction = new TransactionInstruction({
+            keys: [
+                { pubkey: phantom.publicKey, isSigner: true, isWritable: false },       
+                { pubkey: tokenMint, isSigner: false, isWritable: true },          
+                { pubkey: mintAuth, isSigner: false, isWritable: false },                     
+                { pubkey: userAta, isSigner: false, isWritable: true },      
+                { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false }    
+            ],    
+            programId: new PublicKey(nftAddressV2),
+            data: instructionData2.toBuffer()
+        });
+     
+        const transaction2 = tx.add(createInstruction);
 
-const programId = new PublicKey(
-  "G1DCNUQTSGHehwdLCAmRyAG8hf51eCHrLNUqkgGKYASj"
-);
+        transaction2.feePayer = phantom.publicKey;
 
-async function getPDA() {
-  let [pda, bump] = await PublicKey.findProgramAddress(
-    [Buffer.from("metadata")],
-    programId
-  );
+        // 签名并广播交易
+        const { signature2 } = await phantom.signAndSendTransaction(transaction2);
 
-  console.log(`bump: ${bump}, pubkey: ${pda.toBase58()}`);
-  return pda.toBase58();
+        // signature即交易哈希
+        console.log("创建nft: ", signature2);
+
+  } catch (error) {
+    console.log(error);
+    throw new Error(error.message);
+  }
 }
 
+// 查询合约
+function selectContract() {}
 
-async function callContract() {
-  class Assignable {
-    constructor(properties: { [x: string]: any; methods_id?: number; id?: number; description?: string; owner?: string; creator?: string; authorize?: boolean; url?: string; cid?: string; is_mutable?: boolean; }) {
-      Object.keys(properties).map((key) => {
-        return (this[key] = properties[key]);
-      });
+class Assignable {
+    constructor(properties) {
+        Object.keys(properties).map((key) => {
+            return (this[key] = properties[key]);
+        });
     };
-  };
+};
 
-  function intToBool(i: number) {
+function intToBool(i: number) {
     if (i == 0) {
       return false;
     } else {
       return true;
     }
   }
-
+  
   function boolToInt(t: boolean) {
     if (t) {
       return 1;
@@ -326,104 +313,37 @@ async function callContract() {
       return 0;
     }
   }
-
+  
   const boolMapper = {
     encode: boolToInt,
     decode: intToBool,
   };
 
-  class InitializeTokenMintArgs extends Assignable {
+
+class InitializeTokenMintArgs extends Assignable {
     toBuffer() {
-      return Buffer.from(borsh.serialize(InitializeTokenMintArgsSchema, this));
+        return Buffer.from(borsh.serialize(InitializeTokenMintArgsSchema, this));
     }
-  };
-  const InitializeTokenMintArgsSchema = new Map([
+};
+
+const InitializeTokenMintArgsSchema = new Map([
     [
-      InitializeTokenMintArgs, {
-        kind: 'struct',
-        fields: [
-          ["methods_id", "u64"],
-          ["id", "u64"],
-          ["description", "string"],
-          ["owner", "string"],
-          ["creator", "string"],
-          ["authorize", "u8", boolMapper],
-          ["url", "string"],
-          ["cid", "string"],
-          ["is_mutable", "u8", boolMapper],
-        ]
-      }
+    InitializeTokenMintArgs, {
+            kind: 'struct',
+            fields: [
+                ['methods_id', 'u64'],
+                ['id', 'u64'],
+                ['description', 'string'],
+                ['owner', 'string'],
+                ['creator', 'string'],
+                ["authorize", "u8", boolMapper],
+                ['url', 'string'],
+                ['cid', 'string'],
+                ["is_mutable", 'u8', boolMapper]
+            ]
+        }
     ]
-  ]);
-
-
-  class InstructionData extends Assignable {
-    toBuffer() {
-      return Buffer.from(borsh.serialize(InstructionDataSchema, this));
-    }
-  }
-
-  const InstructionDataSchema = new Map([
-    [
-      InstructionData,
-      {
-        kind: "struct",
-        fields: [
-          ["methods_id", "u64"],
-          ["id", "u64"],
-          ["description", "string"],
-          ["owner", "string"],
-          ["creator", "string"],
-          ["authorize", "u8", boolMapper],
-          ["url", "string"],
-          ["cid", "string"],
-          ["is_mutable", "u8", boolMapper],
-        ],
-      },
-    ],
-  ]);
-
-  const nftMint = new InstructionData({
-    methods_id: 2,
-    id: 3,
-    description: "hello world!",
-    owner: "Ee9tjcAXwDeHtVxKePgQos3YqxGo9sxExryCT8a1DFqe",
-    creator: "Ee9tjcAXwDeHtVxKePgQos3YqxGo9sxExryCT8a1DFqe",
-    authorize: true,
-    url: "https://green-sad-canidae-844.mypinata.cloud/ipfs/QmcUgQvRjpgg1qhsfVNE497unLzMVkHbERQcPkCWdx5tyU/0.json",
-    cid: "QmcUgQvRjpgg1qhsfVNE497unLzMVkHbERQcPkCWdx5tyU",
-    is_mutable: false,
-  });
-
-  const instruction = new TransactionInstruction({
-    programId: new PublicKey(
-      "4SqR3DmxCeoLS8pej73CSnNrTmnM58h3WZjS1dksV9ZQ"
-    ),
-    keys: [],
-    data: nftMint.toBuffer(),
-  })
-
-  
-  const transaction = new Transaction();
-  // // 获取最近的区块交易哈希
-  // anyTransaction.recentBlockhash = (
-  //   await connection.getLatestBlockhash('finalized')
-  // ).blockhash;
-
-  let blockhash = (await connection.getLatestBlockhash('finalized')).blockhash;
-  transaction.recentBlockhash = blockhash;
-
-  transaction.feePayer = phantom.publicKey;
-  await transaction.add(instruction)
-  console.log('transaction==>', transaction)
-  // 签名并广播交易
-  const tx = await phantom.signAndSendTransaction(transaction);
-  // console.log('signature==>', signature)
-  // 获取交易签名
-  await connection.getSignatureStatus(tx.signature)
-  console.log('hash==>', tx)
-
-}
+]);
 
 </script>
 
